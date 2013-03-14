ad79cca0a113598b253bcaa757051afde1c41832
Takuya Noaki <nokkii.03@gmail.com>
2013-03-14 10:28:31 +0900

    最初のコミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BikeHackathon::Application.config.secret_token = 'acc6304afdf5ee4ac14fbcd1232576ec297e45b398faa332f51fa35a6512d67fde1b0190eb93c7e3571a07ea946c2820d1af0242443b653a00b203a521957cbc'
